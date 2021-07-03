4c3f9e771f0a92a55197148db17058329ab36651
Félix <77358382+ftdfr@users.noreply.github.com>
2021-07-03 19:12:13 +0200

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+let bouton = document.getElementById("top-button");
+let bouton2 = document.getElementById("top-button2");
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY === 0) {
+        bouton.style.display="none";
+    } else {
+        bouton.style.display="block";
+    }
+  });
+
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY === 0) {
+        bouton.style.display="none";
+    } else {
+        bouton.style.display="block";
+    }
+  });
