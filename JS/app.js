f86ac046405cc82ea771466c2e9ab79514859bdd
Félix <77358382+ftdfr@users.noreply.github.com>
2021-09-06 10:46:21 +0200

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
 let bouton = document.getElementById("top-button");
 const loader = document.querySelector('.loader');
 
+
 window.addEventListener('scroll', () => {
     if(window.scrollY === 0) {
         bouton.style.display="none";
@@ -8,3 +9,16 @@ window.addEventListener('scroll', () => {
         bouton.style.display="block";
     }
   });
+
+document.getElementById("connexion").addEventListener("submit", function(sent){
+    sent.preventDefault();
+    var mdp = document.getElementById("mdp");
+    if (mdp == motdepasse) { 
+    location.replace("http://127.0.0.1:5500/formation.html");
+    } else {
+        alert('Le mot de passe n\'+est pas correct')
+    }
+});
+
+
+
